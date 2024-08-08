1c11b927721c8702a6ba0eebf7693c952f0283c8
NDanczuk <nicolasdanc@gmail.com>
2024-08-08 15:52:27 -0300

    JWT validation config

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Request } from 'express';
 import { sign } from 'jsonwebtoken';
 import { Model } from 'mongoose';
 import { User } from 'src/users/models/users.model';
@@ -14,5 +19,29 @@ export class AuthService {
     return sign({ userId }, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRATION,
     });
+  }
+
+  public async validateUser(userId: string): Promise<User> {
+    const user = await this.usersModel.findOne({ _id: userId });
+    if (!user) {
+      throw new UnauthorizedException('User not found!');
+    }
+    return user;
+  }
+
+  private static jwtExtractor(request: Request): string {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      throw new BadRequestException('Bad request!');
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    return token;
+  }
+
+  public returnjwtExtractor(): (request: Request) => string {
+    return AuthService.jwtExtractor;
   }
 }
